baf0bb80820c7ff6c8b78b15bee0114de3ec6966
san1x2003 <evtyushkin03@mail.ru>
2022-09-15 15:53:08 +0300

    1

---
+++ 3213213213/ConsoleApp17/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(); 
+        }
+    }
+}
